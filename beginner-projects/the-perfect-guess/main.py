6ba1cfcc71e242d97d2ba549e4cdabb3875b2e8f
va1bhav-sh <vaibhavsharma.dev28@gmail.com>
2026-03-17 22:45:38 +0530

    Guess The Number

--- beginner-projects/the-perfect-guess/main.py
+++ beginner-projects/the-perfect-guess/main.py
@@ -2,14 +2,17 @@
 import random
 n=random.randint(1,100)
 a=-1
-guesses=0
+guesses = 0
 while(a!=n):
-    guesses+=1
+    
     a=int(input("Guess a number:"))
     if(a>n):
         print("Lower number please")
-    else:
+        guesses+=1
+    elif(a<n):
         print("Higher number please")
+        guesses+=1
+    
         
     
-print(f"You have guessed the number {n} correctly in {guesses} attempt")
+print(f"You have guessed the number {n} correctly in {guesses} attempt.")
